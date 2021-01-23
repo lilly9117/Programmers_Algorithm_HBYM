a9c9616f4d10fb2fe3ee2cf41f290a2bb50a0729
Yumin <55529646+yourmean@users.noreply.github.com>
2021-01-23 21:28:46 +0900

    210123

---
+++ Yumin/Lv1_크레인인형뽑기게임.py
@@ -0,0 +1,16 @@
+#https://programmers.co.kr/learn/courses/30/lessons/64061
+
+def solution(board, moves):
+    basket = [] #바구니
+    answer=0
+    for m in moves:
+        for i in range(len(board)):
+            if board[i][m-1]!=0:
+                basket.append(board[i][m-1])
+                board[i][m-1] = 0
+
+                if len(basket)>=2 and  basket[-2] == basket[-1]:
+                    answer +=2
+                    del basket[-2:]
+                break
+    return answer
