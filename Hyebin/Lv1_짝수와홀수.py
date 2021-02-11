96a9633733719c19ceb8949ae87162bd58e06fe3
lilly9117 <lilly9117@naver.com>
2021-02-11 23:24:18 +0900

    210211

---
+++ Hyebin/Lv1_짝수와홀수.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = ''
+    if num % 2 == 0:
+        answer = "Even"
+    else:
+        answer = "Odd"
+    return answer
+
+# n = 3
+# print(solution(n))
+# n = 4
+# print(solution(n))
