9c669707e202ec3e3606b08638289cb839a66cbb
lilly9117 <lilly9117@naver.com>
2021-02-11 23:21:02 +0900

    210211

---
+++ Hyebin/Lv1_제일작은수제거하기.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    answer = []
+    if len(arr) == 1:
+        answer.append(-1)
+        return answer
+    else:
+        arr.remove(min(arr))
+        return arr
+
+
+# arr = [4,3,2,1]
+# print(solution(arr))
+
+# arr = [10]
+# print(solution(arr))
