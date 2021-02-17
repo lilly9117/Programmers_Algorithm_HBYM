16a349be84bcebc158ccc022cd16cec4f8e577a6
lilly9117 <lilly9117@naver.com>
2021-02-18 00:58:40 +0900

    210218

---
+++ Hyebin/Lv2_프린터2.py
@@ -0,0 +1,13 @@
+# sol : https://programmers.co.kr/learn/courses/30/lessons/42587/solution_groups?language=python3
+
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
