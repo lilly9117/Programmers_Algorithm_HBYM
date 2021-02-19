76396397f8f70cf72c25ee764bad04d0c39314d8
lilly9117 <lilly9117@naver.com>
2021-02-19 13:22:02 +0900

    210219

---
+++ Hyebin/Lv2_스킬트리.py
@@ -0,0 +1,17 @@
+def solution(skill, skill_trees):
+    answer = len(skill_trees)
+    skill = list(skill)
+    for tree in skill_trees:
+        step_tree = []
+        for s in list(tree):
+            if s in skill:
+                step_tree.append(s)
+        for index, s_t in enumerate(step_tree):
+            if s_t != skill[index]:
+                answer -=1
+                break
+    return answer
+
+# skill = "CBD"
+# skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+# print(solution(skill, skill_trees))
