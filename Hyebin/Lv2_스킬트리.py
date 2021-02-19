faed4193d972031f7388eb174766c12402241748
lilly9117 <lilly9117@naver.com>
2021-02-19 13:25:54 +0900

    210219

--- Hyebin/Lv2_스킬트리.py
+++ Hyebin/Lv2_스킬트리.py
@@ -1,11 +1,14 @@
 def solution(skill, skill_trees):
     answer = len(skill_trees)
     skill = list(skill)
+
     for tree in skill_trees:
         step_tree = []
+
         for s in list(tree):
             if s in skill:
                 step_tree.append(s)
+                
         for index, s_t in enumerate(step_tree):
             if s_t != skill[index]:
                 answer -=1
